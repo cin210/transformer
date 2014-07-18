be8319d4f594b3c1796a717f90985c59698c4b95
Chris Neale <cin210@nyu.edu>
2014-07-18 13:07:59 -0700

    It's Caturday

--- main.ts
+++ main.ts
@@ -5,7 +5,10 @@ html() {
   $("/html/body") {
     # Append a class to the body tag
     add_class("moov")
-
+    # Caturday
+    $$("img"){
+      attr("src", "http://thecatapi.com/api/images/get?format=src")
+    }
     # Sometimes empty blocks break stuff
     $(".") {
     }
