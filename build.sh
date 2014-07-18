f3b0c6b3e964ffc6aeed99f709800970e279a1e9
Chris Neale <cin210@nyu.edu>
2014-07-18 11:45:28 -0700

    Build script

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 export OLD_DYLD=$DYLD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=$GOPATH/clibs/lib
+#GOARCH=amd64 GOOS=linux go build -ldflags -extldflags=-L$DYLD_LIBRARY_PATH
 go build -ldflags -extldflags=-L$DYLD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=$OLD_DYLD
