6ff7e0ffda6ea9022c4a5cab25b96a39dd30869f
Chris Neale <cin210@nyu.edu>
2014-07-21 14:34:14 -0700

    More informative notes

--- transformer.go
+++ transformer.go
@@ -1,5 +1,9 @@
 package main
 
+// This webserver will accept requests on port 3030
+// Visiting any subdomain will return a transformed 
+// html response using the Open Tritium driver.
+
 import(
     "fmt"
     "io/ioutil"
@@ -13,7 +17,11 @@ type Page struct {
     Body []byte
 }
 
+// A request is made to the web server in the form of HOSTNAME:3030/my.cool.site
+// The request is made on behalf of the client, and the response is returned as the Body []byte
+
 func request(path string) (*Page, error){
+    // For now, pass through all subdomains as insecure http connections
     target  := "http://" + path
     u, _    := url.ParseRequestURI(target)
     urlStr  := fmt.Sprintf("%v", u)
@@ -27,13 +35,22 @@ func request(path string) (*Page, error){
     }
 }
 
+// The handler will attempt to make a connection on behalf of the user
+// tritium.Transform will apply the tscript "main.ts" to the response
+// This will parse the body of the response and apply any and all transformations 
+// specified. In this naive example, the src of all images found in the body
+// are replaced with a random cat picture, and the 'moov' class is added to the body tag.
+// See developer.moovweb.com and tritium.io for more applications of Open Tritium
+
 func handler(w http.ResponseWriter, r *http.Request){
     output := ""
     if r.URL.Path[1:] == "" {
         output     = "Server running. Visit /your.domain.here to transform your.domain.here in real time"
     } else {
+	    // load the tscript used by this program.
         tscript, _ := ioutil.ReadFile("main.ts")
         html, _    := request(r.URL.Path[1:])
+		// Invoke a tritium transformation
         output     = tritium.Transform(string(tscript), string(html.Body))
     }
 
